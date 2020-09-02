fb2804a302077b653cfbc1e305b6c575b61c1785
edupugliesi <pugliesiedu@gmail.com>
2020-09-01 22:56:36 -0300

    Iniciando JS

---
+++ index.js
@@ -0,0 +1,5 @@
+const inputManual = document.getElementById('inputManual')
+const outputDados = document.getElementById('outputDados')
+const inserirDados = document.getElementById('inserirDados')
+const exibirDados = document.getElementById('exibirDados')
+
