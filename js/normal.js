d5f4e92ad2802bb1bba0ad8ff5a1e3c6648c71c6
edupugliesi <pugliesiedu@gmail.com>
2020-11-02 19:16:42 -0300

    Probabilidade JavaScript Início
    -Criação dos arquivos JS das variáveis de probabilidade
    -Pequenos ajustes na página HTML referente a variável de probabilidade normal

---
+++ js/normal.js
@@ -0,0 +1,6 @@
+const media = document.getElementById("media")
+const dp = document.getElementById("desvio-padrao")
+const valor = document.getElementById("valor")
+
+const calcular = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
---
+++ js/uniforme.js
@@ -0,0 +1,9 @@
+const pMin = document.getElementById("ponto-minimo")
+const pMax = document.getElementById("ponto-maximo")
+
+const maior = document.getElementById("maior")
+const entre = document.getElementById("entre")
+const menor = document.getElementById("menor")
+
+const calcular = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
