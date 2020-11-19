f2226dbd2d5e9d2e832a18c0c9acc98cb289b49e
edupugliesi <pugliesiedu@gmail.com>
2020-11-19 18:41:53 -0300

    Retiradas algumas linhas que não foram utilizadas na Distribuição Binomial

--- js/binomial.js
+++ js/binomial.js
@@ -7,14 +7,11 @@ const resultado = document.getElementById("resultado")
 
 function binomial(){
     let n = parseFloat(amostra.value)
-    let k = []
+    let k = parseFloat(evento.value)
     let p = parseFloat(sucesso.value)
     let q = parseFloat(fracasso.value)
 
-    k = evento.value.split(";")
-
-    console.log(k, "Elementos presentes no vetor K")
-
+    //Análise Combinatória
     function fatorial(x){
         let fat = 1
         for(let i=x; i>1; i--){
@@ -22,8 +19,6 @@ function binomial(){
         }
         return fat
     }    
-
-    //Análise Combinatória
     
     let fatN = fatorial(n)
     let fatK = fatorial(k)
@@ -35,10 +30,9 @@ function binomial(){
 
     //Distribuição Binomial
     let DB = 0
-    if(k.length == 1){
-        DB = (AC)*(p**k)*(q**(n-k)) * 100
-        resultado.innerHTML = DB.toFixed(2) + "%"
-    }    
+    DB = (AC)*(p**k)*(q**(n-k)) * 100
+    resultado.innerHTML = DB.toFixed(2) + "%"
+       
 }
 
 calcular.addEventListener("click", binomial)
