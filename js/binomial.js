99789a05c34636cebc80fc59554822d85f003cd5
edupugliesi <pugliesiedu@gmail.com>
2020-11-19 18:37:41 -0300

    Distribuição Binomial Refeita
    PS: Fazendo apenas com UM evento de cada vez.

--- js/binomial.js
+++ js/binomial.js
@@ -7,32 +7,38 @@ const resultado = document.getElementById("resultado")
 
 function binomial(){
     let n = parseFloat(amostra.value)
-    let k = parseFloat(evento.value)
+    let k = []
     let p = parseFloat(sucesso.value)
     let q = parseFloat(fracasso.value)
-    /*
+
+    k = evento.value.split(";")
+
+    console.log(k, "Elementos presentes no vetor K")
+
     function fatorial(x){
         let fat = 1
         for(let i=x; i>1; i--){
-            fat = fat * i
+           fat *= i
         }
         return fat
-    }
-    
+    }    
 
     //Análise Combinatória
+    
     let fatN = fatorial(n)
     let fatK = fatorial(k)
     let fatNK = fatorial(n - k)
-    let res = fatN / (fatK * fatNK)
-    */
+    let AC = fatN / (fatK * fatNK)
+    
+    console.log(AC, "Resultado da análise combinatória")
+    
 
     //Distribuição Binomial
     let DB = 0
-    DB = (n/k)*(p**k)*(q**(n-k)) * 100
-
-
-    resultado.innerHTML = DB.toFixed(2) + "%"
+    if(k.length == 1){
+        DB = (AC)*(p**k)*(q**(n-k)) * 100
+        resultado.innerHTML = DB.toFixed(2) + "%"
+    }    
 }
 
 calcular.addEventListener("click", binomial)
